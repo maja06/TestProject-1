90aa552b53b917fc191a0a71ab43a056a15cfdfd
sofija.vlah <vlahovic.sofija@gmail.com>
2019-04-08 12:16:21 +0200

    Fix

--- aspnet-core/src/TestProject.Application/DTO/DeviceTypePropertiesDto.cs
+++ aspnet-core/src/TestProject.Application/DTO/DeviceTypePropertiesDto.cs
@@ -4,9 +4,13 @@ using TestProject.Models;
 
 namespace TestProject.DTO
 {
-    public class DeviceTypePropertiesDto
+    public class DeviceTypePropertiesDto : EntityDto
     {
-        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int? ParentId { get; set; }
         
         public List<DeviceTypePropertyDto> Properties { get; set; } = new List<DeviceTypePropertyDto>();
 
